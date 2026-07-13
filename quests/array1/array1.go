31ed0f9fb5d8021820e2a01dfd73616da45aa385
Zolet-hash <emmanuelsulwe@gmail.com>
2026-07-13 22:00:20 +0300

    wrap up leetcode array 1 quest

--- quests/array1/array1.go
+++ quests/array1/array1.go
@@ -28,3 +28,76 @@ func GetConcatenation(nums []int) []int {
 	}
 	return num2
 }
+
+/* Q2. Shuffle the Array
+
+Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
+
+Return the array in the form [x1,y1,x2,y2,...,xn,yn].
+
+Example 1:
+
+Input: nums = [2,5,1,3,4,7], n = 3
+Output: [2,3,5,4,1,7]
+Explanation: Since x1=2, x2=5, x3=1, y1=3, y2=4, y3=7 then the answer is [2,3,5,4,1,7].
+
+Example 2:
+
+Input: nums = [1,2,3,4,4,3,2,1], n = 4
+Output: [1,4,2,3,3,2,4,1] */
+
+func Shuffle(nums []int, n int) []int {
+	shuffled := make([]int, 0, len(nums))
+
+	for i := range n {
+		j := i + n
+
+		shuffled = append(shuffled, nums[i], nums[j])
+	}
+	return shuffled
+}
+
+// space complexity is O(n) -> space grows as input grows
+// time comlexity is O(n log n)
+
+/* Q3. Max Consecutive Ones
+Solved
+Easy
+
+Given a binary array nums, return the maximum number of consecutive 1's in the array.
+
+
+
+Example 1:
+
+Input: nums = [1,1,0,1,1,1]
+Output: 3
+Explanation: The first two digits or the last three digits are consecutive 1s. The maximum number of consecutive 1s is 3.
+
+Example 2:
+
+Input: nums = [1,0,1,1,0,1]
+Output: 2
+
+
+
+Constraints:
+
+    1 <= nums.length <= 105
+    nums[i] is either 0 or 1.
+*/
+
+func findMaxConsecutiveOnes(nums []int) int {
+	var count, reset, max int
+	for _, num := range nums {
+		if num == 1 {
+			count++
+			if count > max {
+				max = count
+			}
+		} else {
+			count = reset
+		}
+	}
+	return max
+}
