1a7b3fb5aa7ccd125727ec509fef66c10bc6f114
Zolet-hash <emmanuelsulwe@gmail.com>
2026-07-14 15:01:29 +0300

    specify time and space complexities

--- quests/array2/array2.go
+++ quests/array2/array2.go
@@ -51,3 +51,86 @@ func FindErrNums(nums []int) []int {
 	return []int{dup, sorted[max] + 1}
 
 }
+
+// Time complexity is O(n2) -> very expensive
+
+
+/* Q2. How Many Numbers Are Smaller Than the Current Number
+Easy
+Topics
+premium lock iconCompanies
+Hint
+
+Given the array nums, for each nums[i] find out how many numbers in the array are smaller than it. That is, for each nums[i] you have to count the number of valid j's such that j != i and nums[j] < nums[i].
+
+Return the answer in an array.
+
+ 
+
+Example 1:
+
+Input: nums = [8,1,2,2,3]
+Output: [4,0,1,1,3]
+Explanation: 
+For nums[0]=8 there exist four smaller numbers than it (1, 2, 2 and 3). 
+For nums[1]=1 does not exist any smaller number than it.
+For nums[2]=2 there exist one smaller number than it (1). 
+For nums[3]=2 there exist one smaller number than it (1). 
+For nums[4]=3 there exist three smaller numbers than it (1, 2 and 2).
+
+Example 2:
+
+Input: nums = [6,5,4,8]
+Output: [2,1,0,3]
+
+Example 3:
+
+Input: nums = [7,7,7,7]
+Output: [0,0,0,0] */
+
+// time complexity O(n log n) space complexity O(n)
+// sort and use map
+func SmallerNumbersThanCurrent(nums []int) []int {
+	results := make([]int, len(nums))
+	sorted := append([]int(nil), nums...)
+	slices.Sort(sorted)
+
+	first := make(map[int]int) //first occurence
+
+	for i, v := range sorted {
+		if _, ok := first[v]; !ok {
+			first[v] = i
+
+		}
+
+	}
+
+	//Build results using original order
+	for i, v := range nums {
+		results[i] = first[v]
+	}
+
+	return results
+
+}
+
+// Time complexity: O(n2) - 
+// space compexity: O(1) - constant space
+// Ideal for smaller inputs
+func BSmallerNumbersThanCurrent(nums []int) []int {
+	result := make([]int, len(nums))
+
+	for i := range nums {
+		count := 0
+		for j := range nums {
+			if nums[j] < nums[i] {
+				count++
+			}
+		}
+		result[i] = count
+	}
+
+	return result
+}
+
+// Brute force
