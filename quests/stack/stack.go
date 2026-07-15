337d4f69eb872dc19000515f8c0b7a32934f91b4
Zolet-hash <emmanuelsulwe@gmail.com>
2026-07-15 23:09:11 +0300

    add Reverse polish notation leetcode solution
    medium category

--- quests/stack/stack.go
+++ quests/stack/stack.go
@@ -1,26 +1,13 @@
 package stack
 
-import "slices"
+import (
+	"slices"
+	"strconv"
+)
 
 /*
 https://leetcode.com/problems/build-an-array-with-stack-operations/description/?envType=problem-list-v2&envId=dsa-linear-shoal-stack
 */
-
-type Stack struct {
-	items []string
-}
-
-func (s *Stack) Push(data string) {
-	s.items = append(s.items, data)
-}
-
-func (s *Stack) Pop() {
-	if len(s.items) == 0 {
-		return
-	}
-	s.items = s.items[:len(s.items)-1]
-
-}
 
 func BuildArray(target []int, n int) []string {
 	result := []string{}
@@ -35,5 +22,75 @@ func BuildArray(target []int, n int) []string {
 		}
 	}
 	return result
+
+}
+
+type Stack struct {
+	items []int
+}
+
+func (s *Stack) Push(data int) {
+	s.items = append(s.items, data)
+}
+
+func (s *Stack) Pop() {
+	if len(s.items) == 0 {
+		return
+	}
+	s.items = s.items[:len(s.items)-1]
+
+}
+func operate() {
+
+}
+// https://leetcode.com/problems/evaluate-reverse-polish-notation/submissions/2069099778/?envType=problem-list-v2&envId=dsa-linear-shoal-stack
+func isOperator(s string) bool {
+	switch s {
+	case "+", "-", "*", "/":
+		return true
+	}
+	return false
+}
+
+func EvalRPN(tokens []string) int {
+	items := []int{} // stack LIFO
+	for _, tok := range tokens {
+		if !isOperator(tok) {
+			tokInt, _ := strconv.Atoi(tok)
+			items = append(items, tokInt)
+		} else {
+			switch tok {
+			case "/":
+				pop1 := items[len(items)-2]
+				pop2 := items[len(items)-1]
+				items = items[:len(items)-1]
+				items = items[:len(items)-1]
+				op := pop1 / pop2
+				items = append(items, op)
+			case "-":
+				pop1 := items[len(items)-2]
+				pop2 := items[len(items)-1]
+				items = items[:len(items)-1]
+				items = items[:len(items)-1]
+				op := pop1 - pop2
+				items = append(items, op)
+			case "+":
+				pop1 := items[len(items)-2]
+				pop2 := items[len(items)-1]
+				items = items[:len(items)-1]
+				items = items[:len(items)-1]
+				op := pop1 + pop2
+				items = append(items, op)
+			default:
+				pop1 := items[len(items)-2]
+				pop2 := items[len(items)-1]
+				items = items[:len(items)-1]
+				items = items[:len(items)-1]
+				op := pop1 * pop2
+				items = append(items, op)
+			}
+		}
+	}
+	return int(items[0])
 
 }
